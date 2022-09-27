5efeb3e39d90028dbe5f307a9bd068ea08f3201a
Felipe Alves <profelipealves@outlook.com>
2022-09-27 11:30:44 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
+import { useState } from "react";
+
 const App = () => {
+  const [name, setName] = useState('');
+  const [last, setLast] = useState('');
+  const [age, setAge] = useState(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleLast = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLast(e.target.value);
+  };
+
+  const handleAge = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAge(e.target.value);
+  };
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" value={name} onChange={handleChange} />
       </div>
 
       <div>
-        Sobrenome:
-        <input type="text" />
+        Sobrenome: 
+        <input type="text" value={last} onChange={handleLast}/>
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value={age} onChange={handleAge}/>
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {name} {last}, tudo bem?<br/>
+      Você tem {age} anos.
     </div>
   );
 }
