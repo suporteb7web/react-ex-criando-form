e0bb24de3fffb63d0fb04af442dc0f336bad3fc2
Felipe Alves <profelipealves@outlook.com>
2022-09-27 11:33:01 -0300

    exercicio finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [last, setLast] = useState('');
+  const [name, setName] = useState("");
+  const [last, setLast] = useState("");
   const [age, setAge] = useState(0);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,23 +23,20 @@ const App = () => {
         Nome:
         <input type="text" value={name} onChange={handleChange} />
       </div>
-
       <div>
-        Sobrenome: 
-        <input type="text" value={last} onChange={handleLast}/>
+        Sobrenome:
+        <input type="text" value={last} onChange={handleLast} />
       </div>
-
       <div>
         Idade:
-        <input type="text" value={age} onChange={handleAge}/>
+        <input type="text" value={age} onChange={handleAge} />
       </div>
-
-      <hr/>
-
-      Olá {name} {last}, tudo bem?<br/>
+      <hr />
+      Olá {name} {last}, tudo bem?
+      <br />
       Você tem {age} anos.
     </div>
   );
-}
+};
 
 export default App;
